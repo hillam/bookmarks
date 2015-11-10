4791582ad09d5cddec8043598870b6d835d7e00f
Austin <hillam.mail@gmail.com>
2015-11-10 10:04:18 -0500

    test Bookmarks#index

--- bookmarks/bookmarks.php
+++ bookmarks/bookmarks.php
@@ -3,8 +3,17 @@ require_once '../controller.php';
 require_once '../functions.php';
 
 class Bookmarks extends Controller{
-	private static function index($params){
-
+	private static function index($params = null){
+		$results = select('SELECT * FROM bookmarks');
+		foreach ($results as $row){
+			$tags = select(
+					'SELECT * FROM tags
+						INNER JOIN categorizations
+						ON tags.id = categorizations.tag_id
+						WHERE categorizations.bookmark_id = ' . $row['id']);
+			$row['tags'] = $tags;
+		}
+		echo json_encode($row);
 	}
 
 	private static function create($params){
@@ -19,4 +28,6 @@ class Bookmarks extends Controller{
 
 	}
 }
+
+Bookmarks::index();
 ?>
