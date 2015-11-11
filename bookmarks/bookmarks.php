55f38b1f4a36b19aa713f4c2c155d6b8eb839f89
Austin <hillam.mail@gmail.com>
2015-11-11 16:35:30 -0500

    finished Bookmarks#index basic iteration

--- bookmarks/bookmarks.php
+++ bookmarks/bookmarks.php
@@ -4,21 +4,24 @@ require_once '../functions.php';
 
 class Bookmarks extends Controller{
 	protected static function index($params = null){
-		$results = select('SELECT * FROM bookmarks');
+		$results 	= select('SELECT name, url FROM bookmarks');
+		$bookmarks 	= array();
 		foreach ($results as $row){
 			$tags = select(
-					'SELECT * FROM tags
-						INNER JOIN classifications
-						ON tags.id = classifications.tag_id
-						WHERE classifications.bookmark_id = ' . $row['id']);
-			$row['tags'] = $tags;
+				'SELECT * FROM tags
+					INNER JOIN classifications
+					ON tags.id = classifications.tag_id
+					WHERE classifications.bookmark_id = ' . $row['id']);
+			$b = $row;
+			$b['tags'] = $tags;
+			$bookmarks[] = $b;
 		}
 
-		echo json_encode($results);
+		echo json_encode($bookmarks);
 	}
 
 	protected static function create($params){
-
+		var_dump($_GET['tags']);
 	}
 
 	protected static function update($params){
