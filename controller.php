ef8f6c3f0fa26371e38372db439d1e6b3dd4dc9f
Austin <hillam.mail@gmail.com>
2015-11-11 16:17:42 -0500

    debug

--- controller.php
+++ controller.php
@@ -2,7 +2,7 @@
 require_once 'functions.php';
 
 abstract class Controller{
-	public static function action($action, $params = null){
+	public static function action($action, $params = array()){
 		db_connect();
 		forward_static_call_array(array(get_called_class(), $action), $params);
 		disconnect();
