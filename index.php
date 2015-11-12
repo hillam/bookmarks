39401a9390ca5f7976fe691626cf8f8219630aa3
Austin <hillam.mail@gmail.com>
2015-11-11 19:49:23 -0500

    ...

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-+<!DOCTYPE html>
-+<html>
-+	<head>
-+		<meta charset="utf-8">
-+		<title>Bookmarker</title>
-+
-+		<!-- Bootstrap -->
-+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-+	</head>
-+	<body>
-+		<div class='jumbotron'>
-+			<h1>Bookmarks</h1>
-+		</div>
-+	</body>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Bookmarker</title>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class='jumbotron'>
+			<h1>Bookmarks</h1>
+		</div>
+	</body>
  </html>
