4d73383ed0db18ad52feab122eb786162b93c3d2
Austin <hillam.mail@gmail.com>
2015-11-11 19:46:59 -0500

    readme and title page

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bookmarks
+Store your important links in the cloud!
+
+## Domain:
+http://bookmarks-ahill.rhcloud.com/
+
+## Constraints:
+### users can:
+* create account
+* can create/edit/delete bookmarks and tag them
+* tags should be presented as a list of all existing tags, or users can add a new tag
+* filter their books marks by tag
+* see only their own bookmarks
+
+### admin can:
+* delete a user account
--- index.php
+++ index.php
@@ -1,16 +1,15 @@
-<?php
-include_once 'functions.php';
-
-db_connect();
-$result = select('SELECT * FROM bookmarks');
-?>
-	<ul>
-<?php
-foreach ($result as $row){
-?>
-		<li><a href=' <?php $row['url']; ?> '> <?php $row['name']; ?> </a></li>
-<?php
-}
-disconnect();
-?>
-	</ul>
++<!DOCTYPE html>
++<html>
++	<head>
++		<meta charset="utf-8">
++		<title>Bookmarker</title>
++
++		<!-- Bootstrap -->
++		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
++	</head>
++	<body>
++		<div class='jumbotron'>
++			<h1>Bookmarks</h1>
++		</div>
++	</body>
+ </html>
