95ec064352f0ab4060bb853ec3afae069adbee7b
Austin <hillam.mail@gmail.com>
2015-12-09 19:13:43 -0500

    production - no more error reporting

--- functions.php
+++ functions.php
@@ -5,8 +5,6 @@ $error = NULL;
 session_start();
 $current_user = $_SESSION['user'];
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 
